cad3605da9d9e4b3df52e325f5974013b77915eb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-18 14:39:36 +0500

    Initial Commit

---
+++ WeatherAppSimple/WeatherAppSimpleApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherAppSimpleApp.swift
+//  WeatherAppSimple
+//
+//  Created by Elexoft on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSimpleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
