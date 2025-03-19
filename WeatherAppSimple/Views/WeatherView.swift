ba132dca805f3effbe76c27ab207b994e8c87318
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-19 16:04:17 +0500

    Gettin the data from the core data and showing them in the detail view work done

--- WeatherAppSimple/Views/WeatherView.swift
+++ WeatherAppSimple/Views/WeatherView.swift
@@ -10,33 +10,25 @@ import SwiftUI
 struct WeatherView: View {
     // MARK: - PROPERTIES
     
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
     @StateObject var cityManager: AddCityManager = AddCityManager()
+    
+    @FetchRequest(entity: CityDetails.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \CityDetails.name, ascending: true)]) var cityDetails: FetchedResults<CityDetails>
+    
     @State var showAddCityView: Bool = false
+    
     
     // MARK: - BODY
     
     var body: some View {
         NavigationView {
             List {
-                WeatherItemView()
-                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                    .listRowSeparator(.hidden)
-
-                WeatherItemView()
-                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                    .listRowSeparator(.hidden)
-                
-                WeatherItemView()
-                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                    .listRowSeparator(.hidden)
-                
-                WeatherItemView()
-                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                    .listRowSeparator(.hidden)
-                
-                WeatherItemView()
-                    .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                    .listRowSeparator(.hidden)
+                ForEach(self.cityDetails, id: \.self) { cityDetails in
+                    WeatherItemView()
+                        .listRowInsets(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
+                        .listRowSeparator(.hidden)
+                }
             } //: LIST
             .navigationBarTitle("Weather", displayMode: .large)
             .listStyle(.plain)
@@ -62,6 +54,8 @@ struct WeatherView: View {
 // MARK: - PREVIEW
 
 #Preview {
+    let context = PersistenceController.shared.container.viewContext
     WeatherView()
+        .environment(\.managedObjectContext, context)
         .preferredColorScheme(.dark)
 }
