e4d33561738e542d950551a4479d6249c1eada10
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-24 10:18:40 +0500

    SOme work done and bugs gets fixed

--- WeatherAppSimple/Views/WeatherItemView.swift
+++ WeatherAppSimple/Views/WeatherItemView.swift
@@ -14,6 +14,8 @@ struct WeatherItemView: View {
     @StateObject private var weatherFetcher = WeatherFetch()
     @StateObject private var weatherItemManager = WeatherItemManager()
     
+    @State private var isAnimating: Bool = false
+    
     // MARK: - BODY
     
     var body: some View {
@@ -24,9 +26,15 @@ struct WeatherItemView: View {
                         Text(weather.city.name)
                             .font(.system(size: 25, weight: .bold, design: .rounded))
                             .foregroundColor(Color.white)
+                            .offset(y: isAnimating ? 0 : -50)
+                            .opacity(isAnimating ? 1 : 0)
+                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: isAnimating)
                         
                         Text(weatherItemManager.getCurrentTime())
                             .font(.system(size: 15, weight: .medium, design: .rounded))
+                            .offset(y: isAnimating ? 0 : -50)
+                            .opacity(isAnimating ? 1 : 0)
+                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: isAnimating)
                     }
                     
                     Spacer()
@@ -34,16 +42,25 @@ struct WeatherItemView: View {
                     Text("\(Int(weather.list[0].main.temp))°")
                         .font(.system(size: 53, weight: .light, design: .rounded))
                         .foregroundColor(Color.white)
+                        .offset(y: isAnimating ? 0 : -50)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: isAnimating)
                 }
                 
                 HStack {
                     Text(weather.list[0].weather[0].main)
                         .font(.system(size: 16, weight: .medium, design: .rounded))
+                        .offset(y: isAnimating ? 0 : -50)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: isAnimating)
                     
                     Spacer()
                     
                     Text("H:\(Int(weather.list[0].main.temp_max))° L:\(Int(weather.list[0].main.temp_min))°")
                         .font(.system(size: 15, weight: .medium, design: .rounded))
+                        .offset(y: isAnimating ? 0 : -50)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: isAnimating)
                 }
             } else {
                 Text("Loading...")
@@ -59,9 +76,15 @@ struct WeatherItemView: View {
                 .opacity(0.5)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .frame(width: 370, height: 140)
+                .offset(y: isAnimating ? 0 : -80)
+                .opacity(isAnimating ? 1 : 0)
+                .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6), value: isAnimating)
         )
         .onAppear {
             weatherFetcher.fetchWeather(cityName: cityName)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                isAnimating = true
+            }
         }
     }
 }
