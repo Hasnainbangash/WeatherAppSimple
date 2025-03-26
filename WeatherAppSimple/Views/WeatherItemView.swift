3bf0e6f11c300a9f8d123d23135ea33f0481755f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-26 13:08:28 +0500

    Some more work done

--- WeatherAppSimple/Views/WeatherItemView.swift
+++ WeatherAppSimple/Views/WeatherItemView.swift
@@ -30,7 +30,7 @@ struct WeatherItemView: View {
                         Text(weather.city.name)
                             .font(.system(size: 25, weight: .bold, design: .rounded))
                             .foregroundColor(Color.white)
-                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: nameAnimating)
+                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.7), value: nameAnimating)
                             .offset(y: nameAnimating ? 0 : -50)
                             .opacity(nameAnimating ? 1 : 0)
                             .onAppear {
@@ -41,7 +41,7 @@ struct WeatherItemView: View {
                         
                         Text(weatherItemManager.getCurrentTime())
                             .font(.system(size: 15, weight: .medium, design: .rounded))
-                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: timeAnimating)
+                            .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.7), value: timeAnimating)
                             .offset(y: timeAnimating ? 0 : -50)
                             .opacity(timeAnimating ? 1 : 0)
                             .onAppear {
@@ -56,7 +56,7 @@ struct WeatherItemView: View {
                     Text("\(Int(weather.list[0].main.temp))°")
                         .font(.system(size: 53, weight: .light, design: .rounded))
                         .foregroundColor(Color.white)
-                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: tempAnimating)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.7), value: tempAnimating)
                         .offset(y: tempAnimating ? 0 : -50)
                         .opacity(tempAnimating ? 1 : 0)
                         .onAppear {
@@ -69,7 +69,7 @@ struct WeatherItemView: View {
                 HStack {
                     Text(weather.list[0].weather[0].main)
                         .font(.system(size: 16, weight: .medium, design: .rounded))
-                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: weatherConditionAnimating)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.7), value: weatherConditionAnimating)
                         .offset(y: weatherConditionAnimating ? 0 : -50)
                         .opacity(weatherConditionAnimating ? 1 : 0)
                         .onAppear {
@@ -82,7 +82,7 @@ struct WeatherItemView: View {
                     
                     Text("H:\(Int(weather.list[0].main.temp_max))° L:\(Int(weather.list[0].main.temp_min))°")
                         .font(.system(size: 15, weight: .medium, design: .rounded))
-                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.5), value: HLTempAnimating)
+                        .animation(.spring(response: 0.6, dampingFraction: 0.6, blendDuration: 0.6).delay(0.7), value: HLTempAnimating)
                         .offset(y: HLTempAnimating ? 0 : -50)
                         .opacity(HLTempAnimating ? 1 : 0)
                         .onAppear {
@@ -103,7 +103,7 @@ struct WeatherItemView: View {
             ZStack {
                 Image("WeatherImage")
                     .resizable()
-                    .opacity(0.5)
+                    .opacity(0.7)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                     .frame(width: 370, height: 140)
             }
